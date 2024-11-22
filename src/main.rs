4ced26016f282e614c8a65da2b5d859b572ce430
Pavel Zwerschke <pavelzw@gmail.com>
2024-11-22 18:34:36 +0100

    feat: Add pixi environment as CLI argument

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,16 @@ use tracing_log::AsTrace;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    #[arg(long)]
-    prefix: PathBuf,
-
+    /// The pixi environment to inject packages into.
     #[arg(short, long)]
+    environment: Option<String>,
+
+    /// The prefix to inject packages into.
+    #[arg(short, long)]
+    prefix: Option<PathBuf>,
+
+    /// The package to inject into the environment.
+    #[arg(long)]
     package: Vec<PathBuf>,
 
     #[command(flatten)]
@@ -36,7 +42,18 @@ async fn main() -> Result<()> {
     tracing::debug!("Starting pixi-inject CLI");
     tracing::debug!("Parsed CLI options: {:?}", cli);
 
-    let target_prefix = cli.prefix;
+    let target_prefix = match (cli.prefix, cli.environment) {
+        (Some(_), Some(_)) => {
+            return Err(anyhow::anyhow!(
+                "Both --prefix and --environment cannot be provided at the same time."
+            ));
+        },
+        (Some(prefix), None) => prefix,
+        (None, Some(environment)) => PathBuf::from(format!(".pixi/envs/{}", environment)),
+        (None, None) => PathBuf::from(".pixi/envs/default"),
+    };
+    tracing::info!("Using target prefix: {:?}", target_prefix);
+
     let packages = cli.package;
 
     pixi_inject::pixi_inject(target_prefix, packages).await
